2ee74770a90faaebfc5122807d3bb31a425a3581
Anjana Wickramasooriya <anjanajanith2004@gmail.com>
2026-08-25 18:48:23 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,22 @@
+    </main>
+    <footer class="main-footer">
+        <div class="container">
+            <div class="footer-logo">
+                Art<span>Gallery</span>
+            </div>
+            <p style="max-width: 400px; text-align: center; margin-bottom: 2rem;">
+                The premier marketplace for exploring and purchasing extraordinary artworks from talented artists worldwide.
+            </p>
+            <div style="display: flex; gap: 1.5rem; font-size: 1.5rem;">
+                <a href="#" style="color: white;"><ion-icon name="logo-facebook"></ion-icon></a>
+                <a href="#" style="color: white;"><ion-icon name="logo-instagram"></ion-icon></a>
+                <a href="#" style="color: white;"><ion-icon name="logo-twitter"></ion-icon></a>
+            </div>
+            <div class="footer-bottom">
+                &copy; <?php echo date('Y'); ?> ArtStore. All rights reserved. Designed with precision.
+            </div>
+        </div>
+    </footer>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
